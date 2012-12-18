bfdf6ef9c453186d34f6739ab50346774f85c92a
Aaron Bieber <deftly@gmail.com>
2012-12-17 20:23:04 -0700

    added test for ftp - dates are returning incorrectly

---
+++ test/ftptest.js
@@ -0,0 +1,20 @@
+var nmon = require( __dirname + '/../lib/nmon.js' );
+
+var mon = new nmon();
+
+var params = {
+	interval: 3000,
+	name: 'file',
+	path: '/pub/OpenBSD/snapshots/i386/SHA256',
+	server: 'ftp3.usa.openbsd.org',
+  forced_date: new Date( '1900' ),
+  check_on_start: true
+};
+
+mon.create( 'ftp', params );
+
+mon.on( 'file', function( o ) {
+	console.log( 'file has been modified: %s', o.date );
+});
+
+mon.monitor();
